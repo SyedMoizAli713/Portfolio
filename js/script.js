613980b5b2790f7bc7b1e92599992d2415a55c4b
Your Name <you@example.com>
2025-04-08 09:14:44 +0500

    first

---
+++ js/script.js
@@ -0,0 +1,46 @@
+var typed = new Typed(".text", {
+    strings: ["Frontend Developer", "Web Developer", "Graphics Designer"],
+    typeSpeed: 100,
+    backSpeed: 100,
+    backDelay: 1000,
+    loop: true
+});
+
+
+// Skills Section
+
+// Minimal JavaScript for animations
+document.addEventListener('DOMContentLoaded', function() {
+    // Animate circle progress
+    const circleProgressElements = document.querySelectorAll('.circle-progress');
+    
+    setTimeout(() => {
+      circleProgressElements.forEach(element => {
+        const percentage = element.getAttribute('data-percentage');
+        const circle = element.querySelector('.circle-progress-path');
+        const circumference = 2 * Math.PI * 65; // 2πr where r=65
+        
+        // Calculate stroke-dasharray based on percentage
+        const dashArray = (percentage / 100) * circumference;
+        
+        // Set the stroke-dasharray to animate the circle
+        circle.style.strokeDasharray = `${dashArray} ${circumference}`;
+      });
+    }, 500);
+  });
+// Skills Section end
+
+//certifications
+
+//Script to change modal image
+
+  const certificateImgs = document.querySelectorAll(".certificate-img");
+  const modalImg = document.getElementById("modalImage");
+
+  certificateImgs.forEach(img => {
+    img.addEventListener("click", function () {
+      const src = this.getAttribute("data-img");
+      modalImg.setAttribute("src", src);
+    });
+  });
+
